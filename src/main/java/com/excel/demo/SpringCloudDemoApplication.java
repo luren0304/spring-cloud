792e46ea99f34336045927526ba1309bfe6a0b69
luren0304 <luren0304@126.com>
2018-12-19 23:50:35 +0800

    new project

---
+++ src/main/java/com/excel/demo/SpringCloudDemoApplication.java
@@ -0,0 +1,16 @@
+package com.excel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoApplication.class, args);
+	}
+
+}
+
